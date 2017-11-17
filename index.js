2eade94b3406201c8426c3897c5dc205376a0a7c
Tayleron <tayleron@gmail.com>
2017-11-16 23:37:59 -0330

    URL input implemented, HTML linked

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Requirement Initialization
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
@@ -5,25 +6,25 @@ const fs = require('fs');
 const gm = require('gm');
 const request = require('request');
 
-
+//Secondary Initialization
 const port = 8888;
-
 const app = express();
+const upload = multer();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', express.static('public'));
 
 
-
-app.get('/', (req, res) => {
-    gm('image.jpg')
-        .write('original.jpg', (err) => {
+//app POST requests
+app.post('/', upload.array(), (req, res) => {
+    request(req.body)
+        .pipe(fs.createWriteStream('./uploads/image.jpg'));
         return res.status(200).send()
-    });
 });
 
-app.post('/', (req, res) => {
-    gm('image.jpg')
+app.post('/blur', (req, res) => {
+    gm('./uploads/image.jpg')
         .blur(30, 20)
-        .write('edit.jpg', (err) => {
+        .write('./uploads/edit.jpg', (err) => {
         return res.status(200).send()
     });
 });
