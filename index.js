b471e5fec2b9bf799c68400e66e952ccf9f0f388
Tayleron <tayleron@gmail.com>
2017-11-20 12:03:21 -0330

    URL from req.query, error getting x, y params.

--- index.js
+++ index.js
@@ -14,16 +14,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static('public'));
 
 
-//app POST requests
-app.post('/', upload.array(), (req, res) => {
-    request(req.body)
-        .pipe(fs.createWriteStream('./uploads/image.jpg'));
-        return res.status(200).send()
-});
+// //app POST requests
+// app.post('/', upload.array(), (req, res) => {
+//     request(req.body)
+//         .pipe(fs.createWriteStream('./uploads/image.jpg'));
+//         return res.status(200).send()
+// });
 
-app.post('/blur', (req, res) => {
-    gm('./uploads/image.jpg')
-        .blur(30, 20)
+app.post('/blur', upload.array(), (req, res) => {
+    let url = request(req.query.url)
+    let x = request(req.query.x)
+    let y = request(req.query.y)
+    gm(url)
+        .blur(x, y)
         .write('./uploads/edit.jpg', (err) => {
         return res.status(200).send()
     });
