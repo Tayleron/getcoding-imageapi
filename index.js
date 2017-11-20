0bb92446edb5f368c21f496f948890541c8da687
Tayleron <tayleron@gmail.com>
2017-11-20 14:41:50 -0330

    Crop, rotate, and resize fuctions added.

--- index.js
+++ index.js
@@ -28,8 +28,49 @@ app.post('/blur', upload.array(), (req, res) => {
     gm(url)
         .blur(x, y)
         .write(save, (err) => {
-        return res.status(200).send()
+            return res.status(200).send()
     });
+});
+
+app.post('/crop', upload.array(), (req, res) => {
+    let url = request(req.query.url)
+    let height = (req.query.h) //set the height param
+    let width = (req.query.w) //set the width param
+    let x = (req.query.x) //set the x param
+    let y = (req.query.y) //set the y param
+    let save = (req.query.save) // the set save location
+
+    gm(url)
+        .crop(width, height, x, y)
+        .write(save, (err) => {
+            return res.status(200).send()
+        });
+});
+
+app.post('/rotate', upload.array(), (req, res) => {
+    let url = request(req.query.url)
+    let color = (req.query.c) //set color param
+    let degrees = (req.query.d) //set degrees param
+    let save = (req.query.save) // the set save location
+
+    gm(url)
+        .rotate(color, degrees)
+        .write(save, (err) => {
+            return res.status(200).send()
+        });
+});
+
+app.post('/resize', upload.array(), (req, res) => {
+    let url = request(req.query.url)
+    let width = (req.query.w) //set width param
+    let height = (req.query.h) //set degrees param
+    let save = (req.query.save) // the set save location
+
+    gm(url)
+        .resize(width, height)
+        .write(save, (err) => {
+            return res.status(200).send()
+        });
 });
 
 app.listen(port, () => {
